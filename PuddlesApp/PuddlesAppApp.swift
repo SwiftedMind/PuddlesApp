d65c76c8a5d2aee1053b6dffe4ef8f94d8d0820c
Dennis Müller <d3mueller@me.com>
2023-02-04 17:20:57 +0100

    Initial Commit

---
+++ PuddlesApp/PuddlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuddlesAppApp.swift
+//  PuddlesApp
+//
+//  Created by Dennis Müller on 04.02.23.
+//
+
+import SwiftUI
+
+@main
+struct PuddlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
